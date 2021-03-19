4bf764a589d1dbe0af3ba368ccadadb8ec635457
Batool-Al <batool201457@gmail.com>
2021-03-19 20:41:10 +0300

    jh

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -1,10 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login_purple/Screens/Signup/components/or_divider.dart';
-import 'package:flutter_login_purple/components/rounded_input_field.dart';
-import 'package:flutter_login_purple/components/rounded_small_input_field.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_login_purple/Screens/admin_login/components/background.dart';
 
 
 class Body extends StatefulWidget {
@@ -16,124 +16,276 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  // SignUP
+  String _email;
+  String _password;
+  String _currentStateSelected;
+
+  Future<void> _createUser() async{
+    try{
+      UserCredential _userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+    } on FirebaseAuthException catch(e){
+      print("error $e");
+    }catch(e){
+      print("error $e");
+    }
+  }
 
   final _formKey = GlobalKey<FormState>();
-  final listOfPets = ["IT", "Buseniss", "English"];
-  String dropdownValue = 'Cats';
+
+  final List<String> majors = ['IT', 'Business', 'English'];
+  String dropdownValue = 'IT';
+
+  // Controllers
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final idController = TextEditingController();
+  final firstNameController = TextEditingController();
+  final secondNameController = TextEditingController();
+  final majorController = TextEditingController();
+  /// Clear Method
 
-  final referenceDatase = FirebaseDatabase.instance;
+  void clearTextInput(){
+    emailController.clear();
+    passwordController.clear();
+    idController.clear();
+    firstNameController.clear();
+    secondNameController.clear();
+  }
 
-
-
+  final referenceData = FirebaseDatabase.instance;
 
 
   @override
   Widget build(BuildContext context) {
+
     final dbRef = FirebaseDatabase.instance.reference();
+    final userEmail = "StudentInfo";
+    Size size = MediaQuery.of(context).size;
 
-    final userEmail = "Users";
-    return Form(
-      key: _formKey,
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 100),
-            Row(
-              children: [
-                SizedBox(width: 33,),
-                SmallTextInputField(
-                  icon: Icons.account_circle_outlined,
-                  hint: 'First Name',
-                  inputType: TextInputType.name,
-                  inputAction: TextInputAction.next,
+    return Background(
+      child: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 100),
+              /// First Name and Second Name
+              Row(
+                children: [
+                  SizedBox(width: 33,),
+                  Container(
+                    height: size.height * 0.08,
+                    width: size.width * 0.4,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[500].withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: TextFormField(
+                      controller: firstNameController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.perm_identity, color: Colors.white, size: 20,),
+                          labelText: "First Name",
+                          labelStyle: TextStyle(color: Colors.white, fontSize: 13)
+                      ),
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter Valid Name';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 2,),
+                  Container(
+                    height: size.height * 0.08,
+                    width: size.width * 0.4,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[500].withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: TextFormField(
+                      controller: secondNameController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.perm_identity, color: Colors.white, size: 20,),
+                          labelText: "Second Name",
+                          labelStyle: TextStyle(color: Colors.white, fontSize: 13)
+                      ),
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter Valid Name';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 6,),
+              /// ID
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.grey[500].withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(16),
                 ),
-                SmallTextInputField(
-                  icon: Icons.account_circle_outlined,
-                  hint: 'Last Name',
-                  inputType: TextInputType.name,
-                  inputAction: TextInputAction.next,
-                ),
-              ],
-            ),
-            TextInputField(
-              icon: Icons.account_box_outlined,
-              hint: "User ID",
-              inputType: TextInputType.numberWithOptions(decimal: false, signed: false),
-              inputAction: TextInputAction.next,
-            ),
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Enter Email",
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+                child: TextFormField(
+                  controller: idController,
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.perm_identity, color: Colors.white, size: 20,),
+                      labelText: "User ID",
+                      labelStyle: TextStyle(color: Colors.white)
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) => value.isEmpty ? 'Enter Valid ID' : null,
                 ),
               ),
-              // The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Enter Email';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
+              SizedBox(height: 6,),
+              /// Major
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.4,
+                decoration: BoxDecoration(
+                  color: Colors.grey[500].withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: DropdownButtonFormField(
+                 icon: Icon(Icons.arrow_downward) ,
+                  items: ['IT', 'Business', 'English'].map((major) {
+                    return DropdownMenuItem(
+                      value: major,
+                      child: Text('$major',),
+                    );
+                  }).toList(),
+                  onChanged: (String stored){
+                   setState(() {
+                     this._currentStateSelected = stored;
+                   });
+                  },
+                  value: _currentStateSelected,
 
-            SizedBox(height: 20),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: "Enter Password",
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-              // The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please Paswword';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            RaisedButton(
-              color: Colors.lightBlue,
-              onPressed: () {
-                dbRef.child("Users").push().child(userEmail).set(emailController.text).asStream();
-                dbRef.child("Users").push().child(userEmail).set(passwordController.text).asStream();
+              SizedBox(height: 6,),
+              /// Email
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.grey[500].withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  controller: emailController,
+                  onChanged: (value){
+                  _email = value;
+                  print("email: $_email");
+                  },
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                    prefixIcon: Icon(Icons.email_outlined, color: Colors.white, size: 20,),
+                    labelText: "Enter Email",
+                    labelStyle: TextStyle(color: Colors.white)
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) => value.isEmpty ? 'Enter Email' : null,
+                ),
+              ),
+              SizedBox(height: 6,),
+              /// Password
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.grey[500].withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(16),
+                ),
 
-              },
-              child: Text('Submit'),
+                child: TextFormField(
+                  controller: passwordController,
+                  autocorrect: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  onChanged: (value){
+                    _password = value;
+                    print("password is: $_password");
+                  },
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                    prefixIcon: Icon(Icons.lock_outline, color: Colors.white, size: 20,) ,
+                    labelText: "Enter Password",
+                      labelStyle: TextStyle(color: Colors.white)
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) => value.isEmpty ? 'Please Enter Valid Password' : null,
+                ),
+              ),
+              SizedBox(height: size.height * 0.08,),
+              // Button
+              Container(
+                 margin: EdgeInsets.symmetric(vertical: 10),
+                 width: size.width * 0.5,
+                 child: ClipRRect(
+                     borderRadius: BorderRadius.circular(29),
+                     child: RaisedButton(
+                       padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                       color: Colors.lightBlue,
+                       onPressed: () {
+                         dbRef.child("Users").push().child(userEmail).set({
+                           "First Name": firstNameController.text,
+                           "Second Name": secondNameController.text,
+                           "Email": emailController.text,
+                           "password": passwordController.text,
+                           "ID": idController.text,
+                         }).asStream();
+                         _createUser();
+                         clearTextInput();
+                       },
+                       child: Text(
+                        "SIGNUP",
+                       style: TextStyle(color: Colors.white),
+                ),
+              ),
             ),
-
-            OrDivider(),
-          ],
+          ),
+              OrDivider(),
+            ],
+          ),
         ),
-      ),
 
+      ),
     );
   }
   @override
   void dispose() {
     super.dispose();
+    firstNameController.dispose();
+    secondNameController.dispose();
+    idController.dispose();
     passwordController.dispose();
     emailController.dispose();
   }
 }
-
-
-
-class drop extends StatefulWidget {
+class Drop extends StatefulWidget {
   @override
-  _dropState createState() => _dropState();
+  _DropState createState() => _DropState();
 }
 
-class _dropState extends State<drop> {
+class _DropState extends State<Drop> {
   String currentItemSelected ;
   String nameCity = "";
   List listItem = ["ITC", "Business", "English Literature"];
